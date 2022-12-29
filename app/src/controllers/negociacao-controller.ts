eae3b130b532b9257fdba9a5b2c1fabfd7617ed1
Gabriel Simon <gabrielsimon775@gmail.com>
2022-12-29 13:08:09 -0300

    add inspect decorator

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { Inspeciona } from '../decorators/inspeciona.js';
 import { logarTempoExecucao } from '../decorators/logar-tempo-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -20,6 +21,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
+    @Inspeciona()
     @logarTempoExecucao()
     public adiciona(): void {
         const negociacao = Negociacao.criaDe(
