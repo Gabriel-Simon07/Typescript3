bf4332e1dbb37e2468ebbdbbd9175cefb5e4e6f9
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-02 11:11:26 -0300

    [ implemented ] decorator has been implemented

--- app/src/decorators/injetor-dom.ts
+++ app/src/decorators/injetor-dom.ts
@@ -1,10 +1,10 @@
 export function domInject(selector: string) {
-    return function(
-        target: any,
-        propertyKey: string
-    ) {
+    return function(target: any, propertyKey: string) {
         const getter = function() {
+            console.log(`buscando elemento do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
             return document.querySelector(selector);
         }
+
+        Object.defineProperty(target, propertyKey, { get: getter });
     }
 }
