e50f7888071a50c173f22dc3fac4d69c09106517
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-07 19:25:39 -0300

    [implemented] add validation in decorator

--- app/src/decorators/injetor-dom.ts
+++ app/src/decorators/injetor-dom.ts
@@ -1,8 +1,12 @@
 export function domInject(selector: string) {
     return function(target: any, propertyKey: string) {
+        let elemento: HTMLElement;
         const getter = function() {
-            console.log(`buscando elemento do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
-            return document.querySelector(selector);
+            if(!elemento) {
+                elemento = <HTMLElement>document.querySelector(selector);
+                console.log(`buscando elemento do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
+            }
+            return elemento;
         }
 
         Object.defineProperty(target, propertyKey, { get: getter });
