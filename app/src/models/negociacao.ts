310d1af09c1232d558ecc4410ac467cf889cdba1
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-10 12:13:38 -0300

    [implemented] method added for print data in console

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -21,4 +21,8 @@ export class Negociacao {
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
     }
+
+    public paraTexto(): string {
+        return `Data ${this._data}, Quantidade ${this.quantidade}, Valor ${this.valor}`;
+    }
 }
